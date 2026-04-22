9dd6bf92e8e08500e3c00ab9894022a0f025fe09
zitlem <zitlem@users.noreply.github.com>
2026-04-21 22:12:45 -0400

    feat(quick/260421-unm-01): add CncRoute + 3-tab shell and CncScreen with 5 sub-tabs
    - Add CncRoute (@Serializable data object) to AppShell TabRoute sealed interface
    - Replace Boolean last_tab_converter pref with Int last_tab_index (0/1/2)
    - Add third Tab labeled "CNC" with CncRoute navigation in AppShell
    - Add entry<CncRoute> { CncScreen(...) } in entryProvider
    - Create CncScreen.kt with PrimaryScrollableTabRow for 5 sub-tabs (Triangle, SFM/RPM, Thread, Bolt Circle, Keyway)
    - Sub-tab selection persists to cnc_last_tab in acalc_prefs
    - Create stub composables for SfmRpmCalculator, ThreadCalculator, BoltCircleCalculator, KeywayCalculator

---
+++ app/src/main/kotlin/com/acalc/ui/screens/cnc/ThreadCalculator.kt
@@ -0,0 +1,7 @@
+package com.acalc.ui.screens.cnc
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ThreadCalculator(modifier: Modifier = Modifier) {}
